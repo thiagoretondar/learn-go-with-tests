2178317f6db8a5120a144bb3f618c9de0afdb897
thiagoretondar <t.retondar@gmail.com>
2020-01-27 14:22:15 -0300

    fallback when name isn't passed to Hello

--- helloworld/helloworld.go
+++ helloworld/helloworld.go
@@ -1,5 +1,10 @@
 package helloworld
 
+const englishHelloPrefix = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
 }
--- helloworld/helloworld_test.go
+++ helloworld/helloworld_test.go
@@ -3,10 +3,25 @@ package helloworld
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Thiago")
-	want := "Hello, Thiago"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Thiago")
+		want := "Hello, Thiago"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is suplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
 }
