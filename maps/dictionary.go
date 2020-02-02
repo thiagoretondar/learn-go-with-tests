13019c103d287d79ec28f7f4f3c40dbcf11a31fa
thiagoretondar <t.retondar@gmail.com>
2020-02-02 16:53:37 -0300

    verify if word exists before adding it

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -2,7 +2,10 @@ package maps
 
 import "errors"
 
-var ErrNotFound = errors.New("could not find the word you were looking for")
+var (
+	ErrNotFound   = errors.New("could not find the word you were looking for")
+	ErrWordExists = errors.New("cannot add word because it already exists")
+)
 
 type Dictionary map[string]string
 
@@ -14,6 +17,17 @@ func (d Dictionary) Search(word string) (string, error) {
 	return definition, nil
 }
 
-func (d Dictionary) Add(word, definition string) {
-	d[word] = definition
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -1,6 +1,8 @@
 package maps
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
@@ -27,14 +29,30 @@ func TestSearch(t *testing.T) {
 }
 
 func TestAdd(t *testing.T) {
-	dictionary := Dictionary{}
-	dictionary.Add("test", "this is just a test")
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		dictionary.Add("test", "this is just a test")
 
-	want := "this is just a test"
-	got, err := dictionary.Search("test")
+		want := "this is just a test"
+		got, err := dictionary.Search("test")
 
-	assertNoError(t, err)
-	assertStrings(t, got, want)
+		assertNoError(t, err)
+		assertStrings(t, got, want)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+
+		err := dictionary.Add(word, "new test")
+		assertError(t, err, ErrWordExists)
+
+		searchDefinition, err := dictionary.Search(word)
+		assertNoError(t, err)
+
+		assertStrings(t, searchDefinition, definition)
+	})
 }
 
 func assertStrings(t *testing.T, got, want string) {
